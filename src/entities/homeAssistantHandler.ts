b8918c37f3af6d2115109cdf6144709ed851f275
Matt <matthew.larner@me.com>
2022-01-16 14:17:12 +0800

    add channelLevel variable

--- src/entities/homeAssistantHandler.ts
+++ src/entities/homeAssistantHandler.ts
@@ -275,7 +275,8 @@ export const commandsHandler = ({
               dbmanager.dbinsertorupdate((err) => {
                 fade = bridges.area[area].channel['5'].fade * 10;
                 console.log("updated entry from mqtt with", areaNumber, channelNumber, state);
-                const buffer = util.createBuffer([28, areaNumber, channelNumber - 1, 113, 255, fade, 255]);
+                channelLevel = 255;
+                const buffer = util.createBuffer([28, areaNumber, channelNumber - 1, 113, channelLevel, fade, 255]);
                 dynaliteClient.write(Buffer.from(buffer), (err)=>{
                   sendMqttMessageRgbw(_topic, null, state);
                 });
